51f1ac90f81b4f96abb1ee097fffe8ae6aa147b8
Basilio <basilioferraz10@gmail.com>
2017-01-18 08:53:38 -0200

    Finalizando classe method junto de seu test faltando rodar o phpunit

---
+++ src/Destination/Method.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Simonetti\Integrador\Destination;
+
+
+class Method
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var int
+     */
+    protected $identifier;
+
+    /**
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct(string $description, int $identifier, array $param)
+    {
+        $this->description = $description;
+        $this->identifier = $identifier;
+        $this->params = $param;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdentifier(): int
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+}
---
+++ tests/MethodTests.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Simonetti\Integrador\Tests;
+
+use Simonetti\Integrador\Destination\Method;
+
+class MethodTests extends \PHPUnit_Framework_TestCase
+{
+
+    public function testValidateProperties()
+    {
+        $method = new Method('IncluirPessoaJuridica', 12210, ['nome' => 'Basilio Ferraz', 'cpf' => 15717815794]);
+
+        $this->assertEquals('IncluirPessoaJuridica', $method->getDescription());
+        $this->assertEquals(12210, $method->getIdentifier());
+        $this->assertEquals(['nome' => 'Basilio Ferraz', 'cpf' => 15717815794], $method->getParams());
+        $this->assertArrayHasKey('nome', [$method->getParams()]);
+        $this->assertArrayHasKey('cpf', [$method->getParams()]);
+    }
+
+}
