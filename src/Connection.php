0ed510d2450aea48bae3052f6930ac9596e79aea
Ráfaga Queiroz <rafaga.queiroz@gmail.com>
2017-01-18 13:50:09 -0200

    Criando classe Connection com respectivo teste unitario

--- src/Connection.php
+++ src/Connection.php
@@ -1,12 +1,112 @@
 <?php
+namespace Simonetti\IntegradorFinanceiro;
 
 /**
- * Created by PhpStorm.
- * User: rafaga
- * Date: 18/01/17
- * Time: 11:07
+ * Class Connection
+ * @package Simonetti\IntegradorFinanceiro
  */
 class Connection
 {
+    /**
+     * Name of the database/schema
+     * @var string
+     */
+    protected $dbname;
 
+    /**
+     * Username to use when connecting
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * Password to use when connecting
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * Hostname of the database
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * Port of the database
+     * @var integer
+     */
+    protected $port;
+
+    /**
+     * Driver to use when connecting
+     * @var string
+     */
+    protected $driver;
+
+    /**
+     * Connection constructor.
+     * @param string $dbname Name of the database/schema
+     * @param string $user Username to use when connecting
+     * @param string $password Password to use when connecting
+     * @param string $host Hostname of the database
+     * @param int $port Port of the database
+     * @param string $driver Driver to use when connecting
+     */
+    public function __construct($dbname, $user, $password, $host, $port, $driver)
+    {
+        $this->dbname = $dbname;
+        $this->user = $user;
+        $this->password = $password;
+        $this->host = $host;
+        $this->port = $port;
+        $this->driver = $driver;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDbname(): string
+    {
+        return $this->dbname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort(): int
+    {
+        return $this->port;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDriver(): string
+    {
+        return $this->driver;
+    }
 }
