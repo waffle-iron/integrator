bfe2fa7214fe90f13668dcb8021c60d85b840978
Basilio <basilioferraz10@gmail.com>
2017-01-18 09:44:19 -0200

    melhorando teste para simplificar possivel mudanças

--- tests/MethodTest.php
+++ tests/MethodTest.php
@@ -9,11 +9,15 @@ class MethodTest extends \PHPUnit_Framework_TestCase
 
     public function testValidateProperties()
     {
-        $method = new Method('IncluirPessoaJuridica', 12210, ['nome' => 'Basilio Ferraz', 'cpf' => 15717815794]);
+        $description = 'IncluirPessoaJuridica';
+        $identifier = 12210;
+        $param = ['nome' => 'Basilio Ferraz', 'cpf' => 15717815794];
 
-        $this->assertEquals('IncluirPessoaJuridica', $method->getDescription());
-        $this->assertEquals(12210, $method->getIdentifier());
-        $this->assertEquals(['nome' => 'Basilio Ferraz', 'cpf' => 15717815794], $method->getParams());
+        $method = new Method($description, $identifier, $param);
+
+        $this->assertEquals($description, $method->getDescription());
+        $this->assertEquals($identifier, $method->getIdentifier());
+        $this->assertEquals($param, $method->getParams());
         $this->assertArrayHasKey('nome', $method->getParams());
         $this->assertArrayHasKey('cpf', $method->getParams());
     }
