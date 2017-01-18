05089671e5f661dfcd0e70704ef31f2da842e758
Ráfaga Queiroz <rafaga.queiroz@gmail.com>
2017-01-18 13:50:02 -0200

    Criando classe Connection com respectivo teste unitario

---
+++ src/Connection.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rafaga
+ * Date: 18/01/17
+ * Time: 11:07
+ */
+class Connection
+{
+
+}
---
+++ tests/ConnectionTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafaga
+ * Date: 18/01/17
+ * Time: 11:08
+ */
+
+namespace Simonetti\IntegradorFinanceiro\Tests;
+
+
+use Simonetti\IntegradorFinanceiro\Connection;
+
+
+class ConnectionTest extends \PHPUnit_Framework_TestCase
+{
+
+}
