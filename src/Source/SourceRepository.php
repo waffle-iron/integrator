8621c8c27f12f5616ddcb62075f6e14b052eeca6
Basilio <basilioferraz10@gmail.com>
2017-01-26 17:50:54 -0200

    acertando phpdoc da classe source repository

--- src/Source/SourceRepository.php
+++ src/Source/SourceRepository.php
@@ -3,6 +3,10 @@ namespace Simonetti\IntegradorFinanceiro\Source;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class SourceRepository
+ * @package Simonetti\IntegradorFinanceiro\Source
+ */
 class SourceRepository extends EntityRepository
 {
 
